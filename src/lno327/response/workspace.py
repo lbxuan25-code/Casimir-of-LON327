590cb156cb892ca8894529b76ad36ac83bcd2de7
滨萱刘 <lbxuan25@gmail.com>
2026-07-10 12:49:51 +0800

    Use cached material workspace builder

--- src/lno327/response/workspace.py
+++ src/lno327/response/workspace.py
@@ -7,12 +7,14 @@ from lno327.response.finite_q_bdg import (
     finite_q_bdg_response_from_workspace,
     precompute_finite_q_bdg_workspace_from_model_ansatz,
 )
+from lno327.response.finite_q_material import (
+    precompute_finite_q_material_workspace_from_model_ansatz,
+)
 from lno327.response.finite_q_optimized import (
     FiniteQMaterialWorkspace,
     FiniteQQWorkspace,
     finite_q_bdg_response_from_q_workspace,
     finite_q_bdg_responses_from_q_workspace,
-    precompute_finite_q_material_workspace_from_model_ansatz,
     precompute_finite_q_q_workspace,
     primitive_ward_rhs_from_q_workspace,
 )
