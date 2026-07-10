c21c83335be61fb17d32afb4049ef33d869997c2
滨萱刘 <lbxuan25@gmail.com>
2026-07-10 17:50:01 +0800

    Export d-wave nodal quadrature helpers

--- src/lno327/workflows/__init__.py
+++ src/lno327/workflows/__init__.py
@@ -2,6 +2,10 @@
 
 from __future__ import annotations
 
+from lno327.workflows.dwave_nodal_quadrature import (
+    DWaveNodalQuadratureOptions,
+    build_dwave_nodal_quadrature,
+)
 from lno327.workflows.finite_q_engine import (
     BdGPhaseCorrectionError,
     FiniteQEngineOptions,
@@ -20,10 +24,12 @@ from lno327.workflows.finite_q_quadrature import (
 
 __all__ = [
     "BdGPhaseCorrectionError",
+    "DWaveNodalQuadratureOptions",
     "FiniteQEngineOptions",
     "FiniteQQuadratureOptions",
     "bdg_finite_q_response_imag_axis_from_workspace",
     "bdg_finite_q_response_imag_axis",
+    "build_dwave_nodal_quadrature",
     "collective_form_factor",
     "collective_goldstone_counterterm",
     "finite_q_bdg_response_from_ansatz",
