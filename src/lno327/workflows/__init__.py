9e82500a3310500a30fdf49f09d307c1d25205e9
滨萱刘 <lbxuan25@gmail.com>
2026-07-15 04:50:11 +0800

    Export arbitrary-q vector-adaptive workflows

--- src/lno327/workflows/__init__.py
+++ src/lno327/workflows/__init__.py
@@ -2,6 +2,19 @@
 
 from __future__ import annotations
 
+from lno327.workflows.arbitrary_q_vector_adaptive import (
+    AdaptiveConvergenceError,
+    ArbitraryQVectorAdaptiveOptions,
+    ArbitraryQVectorAdaptiveProfile,
+    ArbitraryQVectorAdaptiveResponseCache,
+    HierarchicalMaterialNodeCache,
+    build_hierarchical_material_node_cache,
+    integrate_arbitrary_q_vector_adaptive,
+    integrate_two_plate_angle_batch_vector_adaptive,
+)
+from lno327.workflows.arbitrary_q_vector_adaptive_parallel import (
+    ArbitraryQVectorAdaptiveParallelEvaluator,
+)
 from lno327.workflows.dwave_nodal_quadrature import (
     DWaveNodalQuadratureOptions,
     build_dwave_nodal_quadrature,
@@ -46,6 +59,11 @@ from lno327.workflows.finite_q_quadrature import (
 )
 
 __all__ = [
+    "AdaptiveConvergenceError",
+    "ArbitraryQVectorAdaptiveOptions",
+    "ArbitraryQVectorAdaptiveParallelEvaluator",
+    "ArbitraryQVectorAdaptiveProfile",
+    "ArbitraryQVectorAdaptiveResponseCache",
     "BdGPhaseCorrectionError",
     "DWaveCubatureCell",
     "DWaveNodalQuadratureOptions",
@@ -54,17 +72,21 @@ __all__ = [
     "DWaveVectorAdaptiveOptions",
     "FiniteQEngineOptions",
     "FiniteQQuadratureOptions",
+    "HierarchicalMaterialNodeCache",
     "bdg_finite_q_response_imag_axis_from_workspace",
     "bdg_finite_q_response_imag_axis",
     "build_dwave_nodal_quadrature",
     "build_dwave_periodic_multishift_quadrature",
     "build_dwave_periodic_shift_ensemble",
+    "build_hierarchical_material_node_cache",
     "collective_form_factor",
     "collective_goldstone_counterterm",
     "cubature_cell_gauss_rule",
     "finite_q_bdg_response_from_ansatz",
     "finite_q_quadrature_points",
     "initial_cubature_cells",
+    "integrate_arbitrary_q_vector_adaptive",
+    "integrate_two_plate_angle_batch_vector_adaptive",
     "merge_cell_components_before_schur",
     "merge_shift_components_before_schur",
     "nested_c4_antithetic_shifts",
