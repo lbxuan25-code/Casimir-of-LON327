3e396d29926111b2090bb609068721ebb4716720
滨萱刘 <lbxuan25@gmail.com>
2026-07-15 22:29:36 +0800

    Lock repository layout to one fixed-point transverse runner

--- tests/test_repository_layout_contract.py
+++ tests/test_repository_layout_contract.py
@@ -26,7 +26,6 @@ def test_active_validation_surface_is_grouped() -> None:
     assert (validation_root / "commands/static").is_dir()
     assert (validation_root / "commands/matsubara").is_dir()
     assert (validation_root / "lib/finite_q_validation_models.py").is_file()
-    assert (validation_root / "lib/static_point_diagnostics.py").is_file()
     assert (
         validation_root
         / "commands/matsubara/transverse_point_sweet_spot.py"
@@ -37,8 +36,23 @@ def test_superseded_point_convergence_files_are_absent() -> None:
     validation_root = ROOT / "validation"
     for relative in (
         "commands/static/nk_scan.py",
+        "commands/static/dwave_gauss_outer.py",
+        "commands/static/dwave_orbit_gauss.py",
+        "commands/static/projection_scan.py",
+        "commands/static/quadrature_compare.py",
         "commands/matsubara/positive_point.py",
         "commands/matsubara/arbitrary_q_uniform_refinement_diagnostic.py",
+        "commands/matsubara/dwave_small_xi.py",
+        "commands/matsubara/bond_metric_positive.py",
+        "commands/matsubara/dwave_orbit_adaptive.py",
+        "commands/matsubara/dwave_orbit_panel_adaptive.py",
+        "commands/matsubara/dwave_orbit_evaluator_profile.py",
+        "commands/matsubara/dwave_orbit_integrand_profile.py",
+        "commands/matsubara/dwave_diagonal_width_scan.py",
+        "commands/matsubara/dwave_orbit_gauss_crosscheck.py",
+        "commands/matsubara/dwave_orbit_certification_scan.py",
+        "commands/matsubara/dwave_orbit_certification_scan_parallel.py",
+        "lib/static_point_diagnostics.py",
     ):
         assert not (validation_root / relative).exists(), relative
 
